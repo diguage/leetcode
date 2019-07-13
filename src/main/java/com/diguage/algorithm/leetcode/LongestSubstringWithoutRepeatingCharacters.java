72456c03aafa7645d8c0536362004bb6939a950b
diguage <leejun119@gmail.com>
2019-07-13 10:50:33 +0800

    finish "3. Longest Substring Without Repeating Characters" with sliding window. related #3

--- src/main/java/com/diguage/algorithm/leetcode/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/com/diguage/algorithm/leetcode/LongestSubstringWithoutRepeatingCharacters.java
@@ -6,11 +6,11 @@ import java.util.Set;
 
 /**
  * = 3. Longest Substring Without Repeating Characters
- *
+ * <p>
  * https://leetcode.com/problems/longest-substring-without-repeating-characters/[Longest Substring Without Repeating Characters - LeetCode]
- *
+ * <p>
  * Given a string, find the length of the *longest substring* without repeating characters.
- *
+ * <p>
  * .Example 1:
  * [source]
  * ----
@@ -18,7 +18,7 @@ import java.util.Set;
  * Output: 3
  * Explanation: The answer is "abc", with the length of 3.
  * ----
- *
+ * <p>
  * .Example 2:
  * [source]
  * ----
@@ -26,7 +26,7 @@ import java.util.Set;
  * Output: 1
  * Explanation: The answer is "b", with the length of 1.
  * ----
- *
+ * <p>
  * .Example 3:
  * [source]
  * ----
@@ -41,11 +41,40 @@ import java.util.Set;
  */
 public class LongestSubstringWithoutRepeatingCharacters {
     /**
+     * Runtime: 9 ms, faster than 49.55% of Java online submissions for Longest Substring Without Repeating Characters.
+     * <p>
+     * Memory Usage: 36 MB, less than 99.88% of Java online submissions for Longest Substring Without Repeating Characters.
+     */
+    public int lengthOfLongestSubstring(String s) {
+        if (Objects.isNull(s) || s.length() == 0) {
+            return 0;
+        }
+        int result = 0;
+        int length = s.length();
+        int head = 0;
+        int tail = 0;
+        Set<Character> characters = new HashSet<>(length * 4 / 3);
+        while (head < length && tail < length) {
+            if (!characters.contains(s.charAt(tail))) {
+                characters.add(s.charAt(tail));
+                tail++;
+                if (result < tail - head) {
+                    result = tail - head;
+                }
+            } else {
+                characters.remove(s.charAt(head));
+                head++;
+            }
+        }
+        return result;
+    }
+
+    /**
      * Runtime: 221 ms, faster than 5.02% of Java online submissions for Longest Substring Without Repeating Characters.
      * <p>
      * Memory Usage: 37.3 MB, less than 97.43% of Java online submissions for Longest Substring Without Repeating Characters.
      */
-    public int lengthOfLongestSubstring(String s) {
+    public int lengthOfLongestSubstringWithBruteForce(String s) {
         if (Objects.isNull(s) || s.length() == 0) {
             return 0;
         }
