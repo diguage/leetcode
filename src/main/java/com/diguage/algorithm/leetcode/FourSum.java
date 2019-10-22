27d64c347f4bb96006648998e683ce3cb09af7b8
diguage <leejun119@gmail.com>
2019-10-23 00:05:58 +0800

    finish "18. 4Sum". Fix #18

--- src/main/java/com/diguage/algorithm/leetcode/FourSum.java
+++ src/main/java/com/diguage/algorithm/leetcode/FourSum.java
@@ -1,6 +1,6 @@
 package com.diguage.algorithm.leetcode;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * = 18. 4Sum
@@ -32,7 +32,73 @@ import java.util.List;
  * @since 2018-07-15 00:58
  */
 public class FourSum {
+    /**
+     * Runtime: 4 ms, faster than 94.46% of Java online submissions for 4Sum.
+     *
+     * Memory Usage: 40.7 MB, less than 52.17% of Java online submissions for 4Sum.
+     */
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        return null;
+        int numCount = 4;
+        if (Objects.isNull(nums) || nums.length < numCount) {
+            return Collections.emptyList();
+        }
+
+        Arrays.sort(nums);
+        int length = nums.length;
+        if (target < numCount * nums[0] || numCount * nums[length - 1] < target) {
+            return Collections.emptyList();
+        }
+
+        List<List<Integer>> result = new LinkedList<>();
+        for (int i = 0; i < length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1; j < length - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                int twoSumTarget = target - nums[i] - nums[j];
+                int minTwoSum = nums[j + 1] + nums[j + 2];
+                int maxTwoSum = nums[length - 1] + nums[length - 2];
+                if (twoSumTarget < minTwoSum || maxTwoSum < twoSumTarget) {
+                    continue;
+                }
+                for (int m = j + 1, n = length - 1; m < n; ) {
+                    int twoSum = nums[m] + nums[n];
+                    if (twoSum < twoSumTarget) {
+                        while (m < n && nums[m] == nums[m + 1]) {
+                            m++;
+                        }
+                        m++;
+                    } else if (twoSumTarget < twoSum) {
+                        while (m < n && nums[n - 1] == nums[n]) {
+                            n--;
+                        }
+                        n--;
+                    } else {
+                        result.add(Arrays.asList(nums[i], nums[j], nums[m], nums[n]));
+
+                        while (m < n && nums[m] == nums[m + 1]) {
+                            m++;
+                        }
+                        m++;
+
+                        while (m < n && nums[n - 1] == nums[n]) {
+                            n--;
+                        }
+                        n--;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        FourSum solution = new FourSum();
+        List<List<Integer>> r1 = solution.fourSum(new int[]{1, 0, -1, 0, -2, 2}, 0);
+        System.out.println(Arrays.deepToString(r1.toArray()));
     }
 }
