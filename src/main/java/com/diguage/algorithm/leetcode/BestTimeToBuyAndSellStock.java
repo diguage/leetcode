a8d8d1a86d3e569bde5553aab7ee6e8e05020178
diguage <leejun119@gmail.com>
2020-01-01 23:55:47 +0800

    finish "121. Best Time to Buy and Sell Stock". Fix #121

---
+++ src/main/java/com/diguage/algorithm/leetcode/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,74 @@
+package com.diguage.algorithm.leetcode;
+
+import java.util.Objects;
+
+/**
+ * = 121. Best Time to Buy and Sell Stock
+ *
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/[Best Time to Buy and Sell Stock - LeetCode]
+ *
+ * Say you have an array for which the ith element is the price of a given stock on day i.
+ *
+ * If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+ *
+ * Note that you cannot sell a stock before you buy one.
+ *
+ * .Example 2:
+ * [source]
+ * ----
+ * Input: [7,1,5,3,6,4]
+ * Output: 5
+ * Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+ * Not 7-1 = 6, as selling price needs to be larger than buying price.
+ * ----
+ *
+ * .Example 2:
+ * [source]
+ * ----
+ * Input: [7,6,4,3,1]
+ * Output: 0
+ * Explanation: In this case, no transaction is done, i.e. max profit = 0.
+ * ----
+ *
+ * @author D瓜哥, https://www.diguage.com/
+ * @since 2020-01-01 13:01
+ */
+public class BestTimeToBuyAndSellStock {
+    /**
+     * Runtime: 198 ms, faster than 8.08% of Java online submissions for Best Time to Buy and Sell Stock.
+     *
+     * Memory Usage: 38.5 MB, less than 76.99% of Java online submissions for Best Time to Buy and Sell Stock.
+     */
+    public int maxProfit(int[] prices) {
+        if (Objects.isNull(prices) || prices.length < 2) {
+            return 0;
+        }
+        int result = 0;
+
+        for (int i = 0; i < prices.length - 1; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                int temp = prices[j] - prices[i];
+                if (temp > result) {
+                    result = temp;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        BestTimeToBuyAndSellStock solution = new BestTimeToBuyAndSellStock();
+        int[] a3 = {2, 7, 1, 4};
+        int r3 = solution.maxProfit(a3);
+        System.out.println((r3 == 5) + " : " + r3);
+
+        int[] a1 = {7, 1, 5, 3, 6, 4};
+        int r1 = solution.maxProfit(a1);
+        System.out.println((r1 == 5) + " : " + r1);
+
+
+        int[] a2 = {7, 6, 4, 3, 1};
+        int r2 = solution.maxProfit(a2);
+        System.out.println((r2 == 0) + " : " + r2);
+    }
+}
