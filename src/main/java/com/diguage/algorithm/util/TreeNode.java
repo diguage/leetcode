af8a7be8355ba2e51fc5be1bc8770a7f9cb62ebe
diguage <leejun119@gmail.com>
2020-01-02 19:36:22 +0800

    finish "101. Symmetric Tree". Fix #101

---
+++ src/main/java/com/diguage/algorithm/util/TreeNode.java
@@ -0,0 +1,15 @@
+package com.diguage.algorithm.util;
+
+/**
+ * @author D瓜哥, https://www.diguage.com/
+ * @since 2020-01-02 00:20
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+    }
+}
---
+++ src/main/java/com/diguage/algorithm/util/TreeNodeUtils.java
@@ -0,0 +1,54 @@
+package com.diguage.algorithm.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @author D瓜哥, https://www.diguage.com/
+ * @since 2020-01-02 00:25
+ */
+public class TreeNodeUtils {
+    public static TreeNode buildTree(List<Integer> array) {
+        if (Objects.isNull(array) || array.size() == 0) {
+            return null;
+        }
+
+        TreeNode result = new TreeNode(array.get(0));
+        List<TreeNode> treeNodeList = new ArrayList<>(array.size());
+        treeNodeList.add(result);
+        for (int i = 1; i < array.size(); i++) {
+            TreeNode parent = treeNodeList.get((i - 1) / 2);
+            if (Objects.isNull(parent)) {
+                continue;
+            }
+            Integer value = array.get(i);
+            TreeNode treeNode = null;
+            if (Objects.nonNull(value)) {
+                treeNode = new TreeNode(value);
+                if (i % 2 == 0) {
+                    parent.right = treeNode;
+                } else {
+                    parent.left = treeNode;
+                }
+            }
+            treeNodeList.add(treeNode);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        List<Integer> integers = Arrays.asList(1, 2, 2, 3, 4, 4, 3);
+        TreeNode treeNode = buildTree(integers);
+        System.out.println(mapper.writeValueAsString(treeNode));
+
+        List<Integer> num = Arrays.asList(1, 2, 2, null, 3, null, 3);
+        TreeNode tree = buildTree(num);
+        System.out.println(mapper.writeValueAsString(tree));
+    }
+}
