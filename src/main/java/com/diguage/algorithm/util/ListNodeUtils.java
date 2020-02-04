d24a26974aad69234e3046be9c5528fbc68497f1
diguage <leejun119@gmail.com>
2020-02-04 22:54:56 +0800

    finish "83. Remove Duplicates from Sorted List". Fix #83

--- src/main/java/com/diguage/algorithm/util/ListNodeUtils.java
+++ src/main/java/com/diguage/algorithm/util/ListNodeUtils.java
@@ -63,6 +63,6 @@ public class ListNodeUtils {
         } else {
             System.out.println("(empty)");
         }
-        System.out.println("\n↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑");
+        System.out.println("↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑");
     }
 }
