ea731600457bfe1c9d8ef9eac6aa89ae6efc82a0
diguage <leejun119@gmail.com>
2020-02-05 17:41:40 +0800

    improve “51. N-Queens” solution

--- src/main/java/com/diguage/algorithm/leetcode/_0051_NQueens.java
+++ src/main/java/com/diguage/algorithm/leetcode/_0051_NQueens.java
@@ -1,7 +1,6 @@
 package com.diguage.algorithm.leetcode;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -19,23 +18,55 @@ public class _0051_NQueens {
      */
     public List<List<String>> solveNQueens(int n) {
         int[][] matrix = new int[n][n];
-        List<List<String>> result = new LinkedList<>();
+        List<List<String>> result = new ArrayList<>();
         backtrack(matrix, 0, 0, result);
+        int size = result.size();
+        if (size == 0) {
+            return result;
+        }
+        for (int i = 0; i < size; i++) {
+            List<String> om = result.get(i);
+            // 如果长度是奇数，而且第一行中间是合法位置，则在回溯过程中已经产生了对称解法。
+            if (!(n % 2 == 1 && om.get(0).charAt(getMid(n)) == 'Q')) {
+                List<String> nm = reverseMatrix(om);
+                result.add(nm);
+            }
+        }
+        return result;
+    }
+
+    private List<String> reverseMatrix(List<String> matrix) {
+        List<String> result = new ArrayList<>(matrix.size());
+        for (String s : matrix) {
+            StringBuilder sb = new StringBuilder(s);
+            result.add(sb.reverse().toString());
+        }
         return result;
     }
 
     private void backtrack(int[][] matrix, int y, int step, List<List<String>> result) {
-        if (step == matrix.length) {
+        int length = matrix.length;
+        if (step == length) {
             result.add(matrixToList(matrix));
             return;
         }
-        for (int xi = 0; xi < matrix[0].length; xi++) {
+        for (int xi = 0; xi < length; xi++) {
+            if (y == 0) {
+                int mid = getMid(length);
+                if (mid < xi) {
+                    break;
+                }
+            }
             if (isValid(matrix, y, xi)) {
                 matrix[y][xi] = 1;
                 backtrack(matrix, y + 1, step + 1, result);
                 matrix[y][xi] = 0;
             }
         }
+    }
+
+    private int getMid(int length) {
+        return length % 2 == 0 ? length / 2 - 1 : length / 2;
     }
 
     private List<String> matrixToList(int[][] matrix) {
@@ -87,7 +118,7 @@ public class _0051_NQueens {
     public static void main(String[] args) {
         _0051_NQueens solution = new _0051_NQueens();
 
-        List<List<String>> r1 = solution.solveNQueens(4);
+        List<List<String>> r1 = solution.solveNQueens(14);
         printMatrix(r1);
     }
 
