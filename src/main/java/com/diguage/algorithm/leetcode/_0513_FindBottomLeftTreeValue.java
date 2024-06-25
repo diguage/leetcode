76e4150ca2685073ae103a3489968d1cdc70abad
diguage <leejun119@gmail.com>
2024-06-25 21:18:32 +0800

    完成257

--- src/main/java/com/diguage/algorithm/leetcode/_0513_FindBottomLeftTreeValue.java
+++ src/main/java/com/diguage/algorithm/leetcode/_0513_FindBottomLeftTreeValue.java
@@ -2,11 +2,18 @@ package com.diguage.algorithm.leetcode;
 
 import com.diguage.algorithm.util.TreeNode;
 
+/**
+ * @author D瓜哥 · https://www.diguage.com
+ * @since 2024-06-25 20:55:53
+ */
 public class _0513_FindBottomLeftTreeValue {
   int curHeight = 0;
 
   int curValue = 0;
 
+  /**
+   * 尝试一下深度优先
+   */
   public int findBottomLeftValue(TreeNode root) {
     dfs(root, 0);
     return curValue;
