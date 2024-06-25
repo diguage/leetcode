55fadfdcd27b7a8ec0ec35df937d8f63b0c4e3ed
diguage <leejun119@gmail.com>
2024-06-25 20:39:46 +0800

    完成404

---
+++ src/main/java/com/diguage/algorithm/leetcode/_0404_SumOfLeftLeaves.java
@@ -0,0 +1,30 @@
+package com.diguage.algorithm.leetcode;
+
+import com.diguage.algorithm.util.TreeNode;
+import com.diguage.algorithm.util.TreeNodeUtils;
+
+import java.util.Arrays;
+
+public class _0404_SumOfLeftLeaves {
+  public int sumOfLeftLeaves(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+    int sum = 0;
+    if (root.left != null
+      && root.left.left == null
+      && root.left.right == null) {
+      sum += root.left.val;
+    }
+    int left = sumOfLeftLeaves(root.left);
+    int right = sumOfLeftLeaves(root.right);
+    return sum + left + right;
+  }
+
+  public static void main(String[] args) {
+    TreeNode root = TreeNodeUtils.buildTree(Arrays.asList(3, 9, 20, null, null, 15, 7));
+    _0404_SumOfLeftLeaves solution = new _0404_SumOfLeftLeaves();
+    int result = solution.sumOfLeftLeaves(root);
+    System.out.println(result + " : " + (result == 24));
+  }
+}
