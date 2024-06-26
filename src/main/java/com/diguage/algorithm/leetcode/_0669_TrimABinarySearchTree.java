fb1168fe17ca2ec54fcb83216f7cc17fbfec1899
diguage <leejun119@gmail.com>
2024-06-26 21:42:46 +0800

    完成669

---
+++ src/main/java/com/diguage/algorithm/leetcode/_0669_TrimABinarySearchTree.java
@@ -0,0 +1,27 @@
+package com.diguage.algorithm.leetcode;
+
+import com.diguage.algorithm.util.TreeNode;
+
+/**
+ * @author D瓜哥 · https://www.diguage.com
+ * @since 2024-06-26 21:19:52
+ */
+public class _0669_TrimABinarySearchTree {
+  /**
+   * 参考 https://leetcode.cn/problems/trim-a-binary-search-tree/solutions/1813384/xiu-jian-er-cha-sou-suo-shu-by-leetcode-qe7q1/[669. 修剪二叉搜索树 - 官方题解^]
+   */
+  public TreeNode trimBST(TreeNode root, int low, int high) {
+    if (root == null) {
+      return null;
+    }
+    if (root.val < low) {
+      return trimBST(root.right, low, high);
+    } else if (high < root.val) {
+      return trimBST(root.left, low, high);
+    } else {
+      root.left = trimBST(root.left, low, high);
+      root.right = trimBST(root.right, low, high);
+      return root;
+    }
+  }
+}
