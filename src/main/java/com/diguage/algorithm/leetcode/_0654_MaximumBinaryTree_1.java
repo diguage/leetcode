1c72e64b382a45779bef5dc2069f06a5de95618d
diguage <leejun119@gmail.com>
2024-06-26 16:21:27 +0800

    完成654，整理单调栈资料

---
+++ src/main/java/com/diguage/algorithm/leetcode/_0654_MaximumBinaryTree_1.java
@@ -0,0 +1,42 @@
+package com.diguage.algorithm.leetcode;
+
+import com.diguage.algorithm.util.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @author D瓜哥 · https://www.diguage.com
+ * @since 2024-06-26 14:34:18
+ */
+public class _0654_MaximumBinaryTree_1 {
+  /**
+   * 参考 https://leetcode.cn/problems/maximum-binary-tree/solutions/1762400/zhua-wa-mou-si-by-muse-77-myd7/[654. 最大二叉树 - 图解LeetCode^]
+   *
+   * TODO: 为什么执行效率非常低啊？
+   */
+  public TreeNode constructMaximumBinaryTree(int[] nums) {
+    if (nums == null || nums.length == 0) {
+      return null;
+    }
+    Deque<TreeNode> deque = new ArrayDeque<>();
+    for (int i = 0; i < nums.length; i++) {
+      TreeNode node = new TreeNode(nums[i]);
+      while (!deque.isEmpty()) {
+        TreeNode topNode = deque.peekLast();
+        if (topNode.val > node.val) {
+          deque.addLast(node);
+          topNode.right = node;
+          break;
+        } else {
+          deque.removeLast();
+          node.left = topNode;
+        }
+      }
+      if (deque.isEmpty()) {
+        deque.addLast(node);
+      }
+    }
+    return deque.peek();
+  }
+}
