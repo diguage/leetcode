66e20f241557a8e2b31cd6b666b13ffcc7178717
diguage <leejun119@gmail.com>
2024-07-05 15:25:47 +0800

    完成1109

---
+++ src/main/java/com/diguage/algorithm/leetcode/_1109_CorporateFlightBookings.java
@@ -0,0 +1,27 @@
+package com.diguage.algorithm.leetcode;
+
+/**
+ * @author D瓜哥 · https://www.diguage.com
+ * @since 2024-07-05 15:17:57
+ */
+public class _1109_CorporateFlightBookings {
+  public int[] corpFlightBookings(int[][] bookings, int n) {
+    if (bookings == null || bookings.length == 0) {
+      return new int[n];
+    }
+    int[] flights = new int[n];
+    for (int[] book : bookings) {
+      int start = book[0] - 1;
+      int end = book[1];
+      int val = book[2];
+      flights[start] += val;
+      if (end < n) {
+        flights[end] -= val;
+      }
+    }
+    for (int i = 1; i < n; i++) {
+      flights[i] += flights[i - 1];
+    }
+    return flights;
+  }
+}
