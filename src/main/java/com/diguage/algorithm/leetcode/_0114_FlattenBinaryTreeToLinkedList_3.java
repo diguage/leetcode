68e1c8367a9edd051dce9a72d909160fcf3b76fe
diguage <leejun119@gmail.com>
2024-07-06 17:35:32 +0800

    再次完成114

---
+++ src/main/java/com/diguage/algorithm/leetcode/_0114_FlattenBinaryTreeToLinkedList_3.java
@@ -0,0 +1,35 @@
+package com.diguage.algorithm.leetcode;
+
+import com.diguage.algorithm.util.TreeNode;
+
+/**
+ * = 114. Flatten Binary Tree to Linked List
+ *
+ * https://leetcode.com/problems/flatten-binary-tree-to-linked-list/[Flatten Binary Tree to Linked List - LeetCode]
+ *
+ * @author D瓜哥 · https://www.diguage.com
+ * @since 2020-01-27 23:18
+ */
+public class _0114_FlattenBinaryTreeToLinkedList_3 {
+  /**
+   * @author D瓜哥 · https://www.diguage.com
+   * @since 2024-07-06 17:12:19
+   */
+  public void flatten(TreeNode root) {
+    if (root == null) {
+      return;
+    }
+    while (root != null) {
+      if (root.left != null) {
+        TreeNode mostRight = root.left;
+        while (mostRight.right != null) {
+          mostRight = mostRight.right;
+        }
+        mostRight.right = root.right;
+        root.right = root.left;
+        root.left = null;
+      }
+      root = root.right;
+    }
+  }
+}
---
+++ src/main/java/com/diguage/algorithm/leetcode/_0114_FlattenBinaryTreeToLinkedList_3rec.java
@@ -0,0 +1,39 @@
+package com.diguage.algorithm.leetcode;
+
+import com.diguage.algorithm.util.TreeNode;
+
+/**
+ * = 114. Flatten Binary Tree to Linked List
+ *
+ * https://leetcode.com/problems/flatten-binary-tree-to-linked-list/[Flatten Binary Tree to Linked List - LeetCode]
+ *
+ * @author D瓜哥 · https://www.diguage.com
+ * @since 2020-01-27 23:18
+ */
+public class _0114_FlattenBinaryTreeToLinkedList_3rec {
+  /**
+   * 递归解决方案
+   *
+   * @author D瓜哥 · https://www.diguage.com
+   * @since 2024-07-06 17:32:56
+   */
+  public void flatten(TreeNode root) {
+    if (root == null) {
+      return;
+    }
+    flatten(root.left);
+    flatten(root.right);
+
+    TreeNode left = root.left;
+    TreeNode right = root.right;
+
+    root.left = null;
+    root.right = left;
+
+    TreeNode mostRight = root;
+    while (mostRight.right != null) {
+      mostRight = mostRight.right;
+    }
+    mostRight.right = right;
+  }
+}
