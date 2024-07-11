d208f2d76b6e1093bcbc2e2c5479fc444f4a4f31
diguage <leejun119@gmail.com>
2024-07-11 17:04:24 +0800

    优化698

--- src/main/java/com/diguage/algo/leetcode/_0698_PartitionToKEqualSumSubsets_1.java
+++ src/main/java/com/diguage/algo/leetcode/_0698_PartitionToKEqualSumSubsets_1.java
@@ -19,17 +19,35 @@ public class _0698_PartitionToKEqualSumSubsets_1 {
     if (sum % k != 0) {
       return false;
     }
-    int[] bucket = new int[k];
     int target = sum / k;
+    // 排序：逆序先放大元素，这样减少回溯的可能和次数
+    Arrays.sort(nums);
+    for (int l = 0, r = nums.length - 1; l < r; l++, r--) {
+      int temp = nums[l];
+      nums[l] = nums[r];
+      nums[r] = temp;
+    }
+    // 最大元素大于平均值，也不可能平分，直接返回
+    if (target < nums[0]) {
+      return false;
+    }
+    int[] bucket = new int[k];
     return backtrack(nums, 0, bucket, target);
   }
 
   private boolean backtrack(int[] nums, int start, int[] bucket, int target) {
-    // 遍历完了所有数组
+    // 遍历完数组内所有的数字，那么就成功
     if (start == nums.length) {
       return true;
     }
     for (int i = 0; i < bucket.length; i++) {
+      // 剪枝：如果两个桶的值一样，只要做一个尝试即可
+      // 这里也包含了：第一个数字放在第一个桶的优化点
+      // 解释：最初始时，所有的桶都是0，在执行完第一个桶后，
+      //      由于回溯一加一减，所有的桶还是0，后面的桶就直接跳过了。
+      if (0 < i && bucket[i - 1] == bucket[i]) {
+        continue;
+      }
       // 剪枝：如果该桶总和大于平均值，则跳过
       if (target < nums[start] + bucket[i]) {
         continue;
