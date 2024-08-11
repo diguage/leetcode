b8e936a1f3c2b4d3a052f5838f63476da4eb81f9
diguage <leejun119@gmail.com>
2024-08-11 23:14:19 +0800

    增加解题范式，增加注释

--- src/main/java/com/diguage/algo/leetcode/_0503_NextGreaterElementII.java
+++ src/main/java/com/diguage/algo/leetcode/_0503_NextGreaterElementII.java
@@ -17,11 +17,19 @@ public class _0503_NextGreaterElementII {
     }
     int[] result = new int[nums.length];
     Deque<Integer> stack = new LinkedList<>();
+    // 只需要将数组“拼接”，遍历两遍数组，就可以解决所有元素后继更大元素的问题
+    // 从后向前遍历，再加上单调递增栈，就是时间复杂度为 O(n) 的解决方案
     for (int i = 2 * nums.length - 1; i >= 0; i--) {
+      // 取余即可获取当前需要处理的元素
       int index = i % nums.length;
+      // 在单调栈不为空的情况下，将栈中小于等于当前元素的值都弹出
       while (!stack.isEmpty() && stack.peek() <= nums[index]) {
         stack.pop();
       }
+      // 剩下元素既是比当前元素大的后继元素。为空则是没有更大元素
+      // 这里还有一个隐含变量：
+      // 由于栈是从后向前添加，则栈顶元素距离当前元素更近。
+      // 如果栈不为空，则栈顶元素就是符合条件的元素。
       result[index] = stack.isEmpty() ? -1 : stack.peek();
       stack.push(nums[index]);
     }
