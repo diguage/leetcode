9a9d9e319b3369754ceca5ded4ed34bee9b85b8a
diguage <leejun119@gmail.com>
2024-08-17 14:58:53 +0800

    优化代码，增加图示

--- src/main/java/com/diguage/algo/leetcode/_0870_AdvantageShuffle.java
+++ src/main/java/com/diguage/algo/leetcode/_0870_AdvantageShuffle.java
@@ -1,28 +1,36 @@
 package com.diguage.algo.leetcode;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class _0870_AdvantageShuffle {
   // tag::answer[]
+
   /**
    * @author D瓜哥 · https://www.diguage.com
    * @since 2024-08-16 20:44:53
    */
   public int[] advantageCount(int[] nums1, int[] nums2) {
-    PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[1] - a[1]);
+    // idx 中存的就是每个 nums2 数组中对应的下标
+    Integer[] idx = new Integer[nums2.length];
     for (int i = 0; i < nums2.length; i++) {
-      pq.offer(new int[]{i, nums2[i]});
+      idx[i] = i;
     }
+    // 利用 nums2 中数字的大小，对下标进行排序
+    // 这样可以保持 nums2 数字不变的情况下，对其数字进行排序。
+    Arrays.sort(idx, Comparator.comparingInt(i -> nums2[i]));
     Arrays.sort(nums1);
     int left = 0, right = nums1.length - 1;
     int[] result = new int[nums1.length];
-    while (!pq.isEmpty()) {
-      int[] nums = pq.poll();
-      if (nums[1] < nums1[right]) {
-        result[nums[0]] = nums1[right];
+    for (int i = nums2.length - 1; i >= 0; i--) {
+      int num = nums2[idx[i]];
+      if (num < nums1[right]) {
+        // nums1 能赢就直接上
+        result[idx[i]] = nums1[right];
         right--;
       } else {
-        result[nums[0]] = nums1[left];
+        // nums1 不能赢就选最小值当炮灰
+        result[idx[i]] = nums1[left];
         left++;
       }
     }
