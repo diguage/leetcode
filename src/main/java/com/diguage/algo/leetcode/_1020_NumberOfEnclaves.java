9973377c00b488c5e5f84ff69facdbc3685c503e
diguage <leejun119@gmail.com>
2025-03-03 14:26:09 +0800

    整理1020

---
+++ src/main/java/com/diguage/algo/leetcode/_1020_NumberOfEnclaves.java
@@ -0,0 +1,13 @@
+package com.diguage.algo.leetcode;
+
+public class _1020_NumberOfEnclaves {
+  // tag::answer[]
+  /**
+   * @author D瓜哥 · https://www.diguage.com
+   * @since 2024-07-12 14:37:26
+   */
+  public int numEnclaves(int[][] grid) {
+    return 0;
+  }
+  // end::answer[]
+}
