a129d89558e3d395e0fda1c65762decbfc9ac1c6
diguage <leejun119@gmail.com>
2025-04-06 15:11:32 +0800

    三刷33

---
+++ src/main/java/com/diguage/algo/leetcode/_0033_SearchInRotatedSortedArray_3.java
@@ -0,0 +1,34 @@
+package com.diguage.algo.leetcode;
+
+public class _0033_SearchInRotatedSortedArray_3 {
+  // tag::answer[]
+
+  /**
+   * @author D瓜哥 · https://www.diguage.com
+   * @since 2025-04-06 14:59:46
+   */
+  public int search(int[] nums, int target) {
+    int left = 0, right = nums.length - 1;
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+      if (nums[mid] == target) {
+        return mid;
+      }
+      if (nums[0] <= nums[mid]) {
+        if (nums[left] <= target && target < nums[mid]) {
+          right = mid - 1;
+        } else {
+          left = mid + 1;
+        }
+      } else {
+        if (nums[mid] < target && target <= nums[right]) {
+          left = mid + 1;
+        } else {
+          right = mid - 1;
+        }
+      }
+    }
+    return -1;
+  }
+  // end::answer[]
+}
