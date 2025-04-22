de19948f53276eacde570dde457171d67c91b07e
diguage <leejun119@gmail.com>
2025-04-22 21:30:39 +0800

    二刷530

--- src/main/java/com/diguage/algo/leetcode/_0530_MinimumAbsoluteDifferenceInBst_2.java
+++ src/main/java/com/diguage/algo/leetcode/_0530_MinimumAbsoluteDifferenceInBst_2.java
@@ -2,38 +2,38 @@ package com.diguage.algo.leetcode;
 
 import com.diguage.algo.util.TreeNode;
 
-
-public class _0530_MinimumAbsoluteDifferenceInBST {
+public class _0530_MinimumAbsoluteDifferenceInBst_2 {
   // tag::answer[]
+
   /**
    * @author D瓜哥 · https://www.diguage.com
    * @since 2024-06-26 17:07:39
    */
   public int getMinimumDifference(TreeNode root) {
     int result = Integer.MAX_VALUE;
-    TreeNode cur = root;
-    TreeNode mostRight;
-    TreeNode pre = null;
-    while (cur != null) {
-      mostRight = cur.left;
+    TreeNode prev = null;
+    TreeNode curr = root;
+    while (curr != null) {
+      TreeNode mostRight = curr.left;
       if (mostRight != null) {
-        while (mostRight.right != null && mostRight.right != cur) {
+        while (mostRight.right != null && mostRight.right != curr) {
           mostRight = mostRight.right;
         }
         if (mostRight.right == null) {
-          mostRight.right = cur;
-          cur = cur.left;
+          mostRight.right = curr;
+          curr = curr.left;
           continue;
         } else {
           mostRight.right = null;
         }
       }
-      if (pre != null) {
-        result = Math.min(result, cur.val - pre.val);
+      if (prev != null) {
+        result = Math.min(result, curr.val - prev.val);
       }
-      pre = cur;
-      cur = cur.right;
+      prev = curr;
+      curr = curr.right;
     }
+
     return result;
   }
   // end::answer[]
