6362047c456bf6bb93a4a0bba9022d26b55ffb54
diguage <leejun119@gmail.com>
2025-05-28 22:36:54 +0800

    优化1442

--- src/main/java/com/diguage/algo/leetcode/_1442_CountTripletsThatCanFormTwoArraysOfEqualXor.java
+++ src/main/java/com/diguage/algo/leetcode/_1442_CountTripletsThatCanFormTwoArraysOfEqualXor.java
@@ -1,49 +1,41 @@
 package com.diguage.algo.leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class _1442_CountTripletsThatCanFormTwoArraysOfEqualXor {
   // tag::answer[]
   /**
    * @author D瓜哥 · https://www.diguage.com
    * @since 2025-05-28 21:33:59
    */
-  int result = 0;
-
   public int countTriplets(int[] arr) {
-    backtrack(arr, new ArrayList<>());
+    int length = arr.length;
+    int[] pre = new int[length + 1];
+    for (int i = 0; i < length; i++) {
+      pre[i + 1] = pre[i] ^ arr[i];
+    }
+    int result = 0;
+    // 优化前
+    // for (int i = 0; i < length - 1; i++) {
+    //   for (int j = i + 1; j < length; j++) {
+    //     for (int k = j; k < length; k++) {
+    //       if (pre[i] == pre[k + 1]) {
+    //         result++;
+    //       }
+    //     }
+    //   }
+    // }
+    // 优化后
+    for (int i = 0; i < length - 1; i++) {
+      for (int k = i + 1; k < length; k++) {
+        if (pre[i] == pre[k + 1]) {
+          result += k - i;
+        }
+      }
+    }
     return result;
-  }
-
-  private void backtrack(int[] arr, List<Integer> path) {
-    if (path.size() == 3) {
-      Integer i = path.getFirst();
-      Integer j = path.get(1);
-      Integer k = path.getLast();
-      int a = arr[i];
-      for (int l = i + 1; l < j; l++) {
-        a ^= arr[l];
-      }
-      int b = arr[j];
-      for (int m = j + 1; m <= k; m++) {
-        b ^= arr[m];
-      }
-      if (a == b) {
-        result++;
-      }
-      return;
-    }
-    int idx = path.isEmpty() ? 0 : (path.size() == 1 ? path.getLast() + 1 : path.getLast());
-    for (int i = idx; i < arr.length; i++) {
-      path.add(i);
-      backtrack(arr, path);
-      path.removeLast();
-    }
   }
   // end::answer[]
   public static void main(String[] args) {
     new _1442_CountTripletsThatCanFormTwoArraysOfEqualXor()
-      .countTriplets(new int[]{218, 218});
+      .countTriplets(new int[]{7, 7, 7, 7});
   }
 }
