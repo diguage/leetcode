0d44a5d05359db91a6d009ac4e2a32e74fca9e8f
diguage <leejun119@gmail.com>
2025-06-02 08:19:17 +0800

    优化1209

--- src/main/java/com/diguage/algo/leetcode/_1209_RemoveAllAdjacentDuplicatesInStringIi.java
+++ src/main/java/com/diguage/algo/leetcode/_1209_RemoveAllAdjacentDuplicatesInStringIi.java
@@ -4,37 +4,24 @@ public class _1209_RemoveAllAdjacentDuplicatesInStringIi {
   // tag::answer[]
 
   /**
-   * 暴力解法： 通过 19 / 21 个测试用例。
-   *
    * @author D瓜哥 · https://www.diguage.com
    * @since 2025-06-02 07:27:39
    */
   public String removeDuplicates(String s, int k) {
-    if (s == null || s.length() < k) {
-      return s;
-    }
     StringBuilder sb = new StringBuilder(s);
-    int length = sb.length();
-    int index = 0;
-    while (index < length) {
-      char c = sb.charAt(index);
-      int fast = index + 1;
-      while (fast < length && c == sb.charAt(fast) && fast - index < k) {
-        fast++;
-      }
-      int dc = fast - index;
-      if (fast <= length && dc >= k) {
-        sb.replace(index, fast, "");
-        length = sb.length();
+    int[] count = new int[s.length()];
+    for (int i = 0; i < sb.length(); i++) {
+      if (i == 0 || sb.charAt(i) != sb.charAt(i - 1)) {
+        count[i] = 1;
       } else {
-        index = fast;
+        count[i] = count[i - 1] + 1;
+        if (count[i] == k) {
+          sb.delete(i - k + 1, i + 1);
+          i -= k;
+        }
       }
     }
-    if (sb.length() == s.length()) {
-      return s;
-    } else {
-      return removeDuplicates(sb.toString(), k);
-    }
+    return sb.toString();
   }
 
   // end::answer[]
