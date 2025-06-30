9bff998e94f3d519e464c99aad779d8671249e5a
diguage <leejun119@gmail.com>
2025-06-30 21:15:28 +0800

    优化214

--- src/main/java/com/diguage/algo/leetcode/_0214_ShortestPalindrome.java
+++ src/main/java/com/diguage/algo/leetcode/_0214_ShortestPalindrome.java
@@ -4,6 +4,10 @@ public class _0214_ShortestPalindrome {
   // tag::answer[]
 
   /**
+   * 优化：使用 reversed.substring(index) 代替逐个字母比较。
+   * <p>
+   * 优化前：通过 124 / 126 个测试用例，超时！
+   *
    * @author D瓜哥 · https://www.diguage.com
    * @since 2025-06-30 20:35:57
    */
@@ -15,20 +19,15 @@ public class _0214_ShortestPalindrome {
     int index = 0;
     int length = s.length();
     while (index < length) {
-      int i = index;
-      int j = 0;
-      while (i < length && reversed.charAt(i) == s.charAt(j)) {
-        i++;
-        j++;
-      }
-      if (i == length) {
-        break;
+      if (reversed.substring(index).equals(s.substring(0, length - index))) {
+        return reversed.substring(0, index) + s;
       }
       index++;
     }
     return reversed.substring(0, index) + s;
   }
   // end::answer[]
+
   public static void main(String[] args) {
     System.out.println(new _0214_ShortestPalindrome().shortestPalindrome("abcd"));
   }
