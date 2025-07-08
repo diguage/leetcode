656372b3ad1c2ae632ba376459484cbd0c3ba223
diguage <leejun119@gmail.com>
2025-07-08 20:07:02 +0800

    一刷292

---
+++ src/main/java/com/diguage/algo/leetcode/_0292_NimGame.java
@@ -0,0 +1,13 @@
+package com.diguage.algo.leetcode;
+
+public class _0292_NimGame {
+  // tag::answer[]
+  /**
+   * @author D瓜哥 · https://www.diguage.com
+   * @since 2025-07-08 20:01:00
+   */
+  public boolean canWinNim(int n) {
+    return n % 4 != 0;
+  }
+  // end::answer[]
+}
