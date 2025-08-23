9a9b90138673a7c52b86b672dbcfa13fc4073b91
diguage <leejun119@gmail.com>
2025-08-23 22:03:32 +0800

    一刷457

---
+++ src/main/java/com/diguage/algo/leetcode/_0457_CircularArrayLoop.java
@@ -0,0 +1,42 @@
+package com.diguage.algo.leetcode;
+
+import java.util.Objects;
+
+public class _0457_CircularArrayLoop {
+  // tag::answer[]
+
+  /**
+   * @author D瓜哥 · https://www.diguage.com
+   * @since 2025-08-23 21:19:58
+   */
+  public boolean circularArrayLoop(int[] nums) {
+    int index = 2000 * nums.length;
+    int base = 100000;
+    for (int i = index; i < index + nums.length; i++) {
+      boolean circular = backtrack(nums, i, base, nums[i % nums.length] > 0, 0, null);
+      if (circular) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private boolean backtrack(int[] nums, int index, int base, boolean positive, int step, Integer last) {
+    int i = index % nums.length;
+    int num = nums[i];
+    if (positive ? num > base : 1000 < num && num < base) {
+      return step > 1 && last != null && !Objects.equals(last, i);
+    } else if ((num > 0) != positive) {
+      return false;
+    }
+    nums[i] = base + num;
+    boolean result = backtrack(nums, index + num, base, positive, step + 1, i);
+    nums[i] = num;
+    return result;
+  }
+  // end::answer[]
+
+  public static void main(String[] args) {
+    new _0457_CircularArrayLoop().circularArrayLoop(new int[]{-1, -2, -3, -4, -5});
+  }
+}
