d0c6cbe09d4788f73e818198c52e6f134b503efc
diguage <leejun119@gmail.com>
2025-08-30 22:52:05 +0800

    优化470

--- src/main/java/com/diguage/algo/leetcode/_0470_ImplementRand10UsingRand7.java
+++ src/main/java/com/diguage/algo/leetcode/_0470_ImplementRand10UsingRand7.java
@@ -7,12 +7,30 @@ public class _0470_ImplementRand10UsingRand7 {
    * @since 2025-08-30 21:59:24
    */
   public int rand10() {
-    int sum = 0;
-    for (int i = 0; i < 10; i++) {
-      sum += rand7();
+    while (true) {
+      int a = rand7();
+      int b = rand7();
+      int num = (a - 1) * 7 + b; // [1, 7*7]
+      if (num <= 40) {
+        return num % 10 + 1;
+      }
+
+      a = num - 40; // rand9
+      b = rand7();
+      num = (a - 1) * 7 + b; // [1, 9*7]
+      if (num <= 60) {
+        return num % 10 + 1;
+      }
+
+      a = num - 60; // rand3
+      b = rand7();
+      num = (a - 1) * 7 + b; // [1, 3*7]
+      if (num <= 30) {
+        return num % 10 + 1;
+      }
     }
-    return sum % 10 + 1;
   }
+
   // end::answer[]
   public int rand7() {
     return 0;
