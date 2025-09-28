7be8c92d7655cd78230aac88e0fcd6b07a5d72ac
diguage <leejun119@gmail.com>
2025-09-28 21:44:00 +0800

    三刷120

---
+++ src/main/java/com/diguage/algo/leetcode/_0120_Triangle_3.java
@@ -0,0 +1,31 @@
+package com.diguage.algo.leetcode;
+
+import java.util.List;
+
+public class _0120_Triangle_3 {
+  // tag::answer[]
+  /**
+   * @author D瓜哥 · https://www.diguage.com
+   * @since 2025-09-28 21:13:30
+   */
+  public int minimumTotal(List<List<Integer>> triangle) {
+    Integer[] dp = triangle.getLast().toArray(new Integer[0]);
+    for (int i = 0; i < triangle.size() - 1; i++) {
+      int idx = triangle.size() - i - 2;
+      List<Integer> curr = triangle.get(idx);
+      for (int j = 0; j <= idx; j++) {
+        dp[j] = curr.get(j) + Math.min(dp[j], dp[j + 1]);
+      }
+    }
+    return dp[0];
+  }
+  // end::answer[]
+
+  static void main() {
+    new _0120_Triangle_3().minimumTotal(List.of(
+      List.of(2),
+      List.of(3, 4),
+      List.of(6, 5, 7),
+      List.of(4, 1, 8, 3)));
+  }
+}
