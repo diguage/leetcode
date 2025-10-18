910dd3cd353275484b9ff591b5794b134d53c354
diguage <leejun119@gmail.com>
2025-10-18 22:56:34 +0800

    优化二刷283

--- src/main/java/com/diguage/algo/leetcode/_0283_MoveZeroes_2.java
+++ src/main/java/com/diguage/algo/leetcode/_0283_MoveZeroes_2.java
@@ -1,28 +1,21 @@
 package com.diguage.algo.leetcode;
 
+import java.util.Arrays;
+
 public class _0283_MoveZeroes_2 {
   // tag::answer[]
-
   /**
    * @author D瓜哥 · https://www.diguage.com
    * @since 2025-10-18 22:33:09
    */
   public void moveZeroes(int[] nums) {
-    int slow = 0, fast = 0;
-    while (fast < nums.length) {
-      if (nums[fast] == 0 || fast == slow) {
-        fast++;
-        continue;
+    int index = 0;
+    for (int num : nums) {
+      if (num != 0) {
+        nums[index++] = num;
       }
-      if (nums[slow] != 0) {
-        slow++;
-        continue;
-      }
-      nums[slow] = nums[fast];
-      nums[fast] = 0;
-      slow++;
-      fast++;
     }
+    Arrays.fill(nums, index, nums.length, 0);
   }
   // end::answer[]
 }
