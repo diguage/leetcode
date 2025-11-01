5060cfaa828a785b999c16f4d3357e8eda89cbec
diguage <leejun119@gmail.com>
2025-11-01 22:37:15 +0800

    四刷153

---
+++ src/main/java/com/diguage/algo/leetcode/_0153_FindMinimumInRotatedSortedArray_4.java
@@ -0,0 +1,34 @@
+package com.diguage.algo.leetcode;
+
+public class _0153_FindMinimumInRotatedSortedArray_4 {
+  // tag::answer[]
+
+  /**
+   * @author D瓜哥 · https://www.diguage.com
+   * @since 2025-11-01 21:57:23
+   */
+  public int findMin(int[] nums) {
+    int left = 0, right = nums.length - 1;
+    while (left < right) {
+      int mid = left + (right - left) / 2;
+      if (nums[0] <= nums[mid] && nums[mid] <= nums[right]) {
+        right = mid;
+      } else if (nums[0] <= nums[mid]) {
+        left = mid + 1;
+      } else {
+        if (nums[mid] < nums[nums.length - 1]) {
+          right = mid;
+        } else {
+          left = mid + 1;
+        }
+      }
+    }
+    return nums[left];
+  }
+
+  // end::answer[]
+  public static void main(String[] args) {
+    new _0153_FindMinimumInRotatedSortedArray_4()
+      .findMin(new int[]{11, 13, 15, 17});
+  }
+}
