d20e7c49d09035be792ca4672b0a9a3dd3f6014c
diguage <leejun119@gmail.com>
2025-11-07 22:55:35 +0800

    优化128

--- src/main/java/com/diguage/algo/leetcode/_0128_LongestConsecutiveSequence_2.java
+++ src/main/java/com/diguage/algo/leetcode/_0128_LongestConsecutiveSequence_2.java
@@ -7,41 +7,29 @@ public class _0128_LongestConsecutiveSequence_2 {
   // tag::answer[]
 
   /**
+   * 优化前，通过 73 / 83 个测试用例。
+   *
    * @author D瓜哥 · https://www.diguage.com
    * @since 2020-02-07 23:56
    */
   public int longestConsecutive(int[] nums) {
-    if (nums == null || nums.length == 0) {
-      return 0;
-    }
     Set<Integer> set = new HashSet<>(nums.length);
     for (int num : nums) {
       set.add(num);
     }
-    int result = 1;
-    int left = 0, right = -1;
-    while (!set.isEmpty()) {
-      if (left > right) {
-        Integer next = set.iterator().next();
-        set.remove(next);
-        left = right = next;
-      } else {
-        int size = right - left + 1;
-        int pre = left - 1;
-        boolean hasPre = set.remove(pre);
-        if (hasPre) {
-          left = pre;
-        }
-        int next = right + 1;
-        boolean hasNext = set.remove(next);
-        if (hasNext) {
-          right = next;
-        }
-        result = Math.max(result, right - left + 1);
-        if (size == right - left + 1) {
-          left = 0;
-          right = -1;
-        }
+    int result = 0;
+    for (int num : set) {
+      if (set.contains(num - 1)) {
+        continue;
+      }
+      int count = 1;
+      for (int i = num + 1; set.contains(i); i++) {
+        count++;
+      }
+      result = Math.max(result, count);
+      // 已经有一个连续数列超过了一半，则不会出现更大的数列了。
+      if (result * 2 >= set.size()) {
+        break;
       }
     }
     return result;
