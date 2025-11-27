c094bed2c0c2be22dda1b564bd6521a8a5a3e056
diguage <leejun119@gmail.com>
2025-11-27 23:20:08 +0800

    优化34

--- src/main/java/com/diguage/algo/leetcode/_0034_FindFirstAndLastPositionOfElementInSortedArray_4.java
+++ src/main/java/com/diguage/algo/leetcode/_0034_FindFirstAndLastPositionOfElementInSortedArray_4.java
@@ -2,12 +2,20 @@ package com.diguage.algo.leetcode;
 
 public class _0034_FindFirstAndLastPositionOfElementInSortedArray_4 {
   // tag::answer[]
-
   /**
    * @author D瓜哥 · https://www.diguage.com
    * @since 2025-11-27 22:43:47
    */
   public int[] searchRange(int[] nums, int target) {
+    int left = binarySearch(nums, target);
+    if (left == nums.length || nums[left] != target) {
+      return new int[]{-1, -1};
+    }
+    int right = binarySearch(nums, target + 1);
+    return new int[]{left, right - 1};
+  }
+
+  private int binarySearch(int[] nums, int target) {
     int left = 0, right = nums.length - 1;
     while (left <= right) {
       int mid = left + (right - left) / 2;
@@ -20,25 +28,7 @@ public class _0034_FindFirstAndLastPositionOfElementInSortedArray_4 {
         right = mid - 1;
       }
     }
-    if (right < 0 || left == nums.length || nums[left] != target) {
-      return new int[]{-1, -1};
-    }
-    int a = left;
-
-    left = 0;
-    right = nums.length - 1;
-    while (left <= right) {
-      int mid = left + (right - left) / 2;
-      // 循环不变量：
-      // nums[left-1] >= target
-      // nums[right+1] < target
-      if (target < nums[mid]) {
-        right = mid - 1;
-      } else {
-        left = mid + 1;
-      }
-    }
-    return new int[]{a, right};
+    return left;
   }
   // end::answer[]
 }
