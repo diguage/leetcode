d9e9b08cfd253e0734aaac6b91cda4e8d620cdf8
diguage <leejun119@gmail.com>
2025-12-21 17:17:14 +0800

    消除全局变量

--- src/main/java/com/diguage/algo/leetcode/_0980_UniquePathsIii_2.java
+++ src/main/java/com/diguage/algo/leetcode/_0980_UniquePathsIii_2.java
@@ -7,8 +7,6 @@ public class _0980_UniquePathsIii_2 {
    * @author D瓜哥 · https://www.diguage.com
    * @since 2025-12-21 16:39:19
    */
-  private int result = 0;
-
   public int uniquePathsIII(int[][] grid) {
     int[] start = new int[2];
     int[] end = new int[2];
@@ -26,36 +24,37 @@ public class _0980_UniquePathsIii_2 {
         }
       }
     }
-    backtrack(grid, start, end, step + 1);
-    return result;
+    return backtrack(grid, start, end, step + 1);
   }
 
-  private void backtrack(int[][] grid, int[] start, int[] end, int step) {
+  private int backtrack(int[][] grid, int[] start, int[] end, int step) {
     int column = start[0];
     int row = start[1];
     if (step == 0 && column == end[0] && row == end[1]) {
-      result++;
-      return;
+      return 1;
     }
     if (column < 0 || column >= grid.length
       || row < 0 || row >= grid[column].length
       || step < 0
       || (grid[column][row] != 1 && grid[column][row] != 0)) {
-      return;
+      return 0;
     }
     int origin = grid[column][row];
     int nextStep = step - 1;
     grid[column][row] = Integer.MAX_VALUE;
+    int result = 0;
     // 上
-    backtrack(grid, new int[]{column - 1, row}, end, nextStep);
+    result += backtrack(grid, new int[]{column - 1, row}, end, nextStep);
     // 下
-    backtrack(grid, new int[]{column + 1, row}, end, nextStep);
+    result += backtrack(grid, new int[]{column + 1, row}, end, nextStep);
     // 左
-    backtrack(grid, new int[]{column, row - 1}, end, nextStep);
+    result += backtrack(grid, new int[]{column, row - 1}, end, nextStep);
     // 右
-    backtrack(grid, new int[]{column, row + 1}, end, nextStep);
+    result += backtrack(grid, new int[]{column, row + 1}, end, nextStep);
 
     grid[column][row] = origin;
+
+    return result;
   }
   // end::answer[]
 
