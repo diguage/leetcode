2bad6cf71766bcd6451282347a45b6e7355b7890
diguage <leejun119@gmail.com>
2026-02-01 23:27:37 +0800

    优化212

--- src/main/java/com/diguage/algo/leetcode/_0212_WordSearchIi_2.java
+++ src/main/java/com/diguage/algo/leetcode/_0212_WordSearchIi_2.java
@@ -6,56 +6,87 @@ public class _0212_WordSearchIi_2 {
   // tag::answer[]
 
   /**
-   * 回溯（63/65）
+   * 回溯（63/65）-> 前缀树+回溯（65.92%）
    *
    * @author D瓜哥 · https://www.diguage.com
    * @since 2026-02-01 22:18:24
    */
   public List<String> findWords(char[][] board, String[] words) {
-    List<String> result = new ArrayList<>();
-    Map<Character, List<int[]>> map = new HashMap<>();
+    Set<String> result = new HashSet<>();
+    Trie trie = new Trie();
+    for (String word : words) {
+      trie.addWord(word);
+    }
     for (int r = 0; r < board.length; r++) {
       for (int c = 0; c < board[r].length; c++) {
-        map.computeIfAbsent(board[r][c], _ -> new ArrayList<>())
-          .add(new int[]{r, c});
+        backtrack(board, r, c, trie, result);
       }
     }
-    for (String word : words) {
-      List<int[]> loc = map.getOrDefault(word.charAt(0), Collections.emptyList());
-      for (int[] l : loc) {
-        if (backtrack(board, l[0], l[1], word, 0, result)) {
-          break;
-        }
-      }
-    }
-    return result;
+
+    return new ArrayList<>(result);
   }
 
   private boolean backtrack(char[][] board, int row, int column,
-                            String word, int index,
-                            List<String> result) {
-    if (index >= word.length()) {
-      result.add(word);
-      return true;
-    }
+                            Trie trie, Set<String> result) {
     if (row < 0 || row >= board.length
       || column < 0 || column >= board[row].length
-      || board[row][column] != word.charAt(index)) {
+      || board[row][column] == '.'
+      || trie.tree[board[row][column] - 'a'] == null) {
       return false;
     }
-
     char c = board[row][column];
     board[row][column] = '.';
+    Trie curr = trie.tree[c - 'a'];
+    if (curr.isEnd) {
+      result.add(curr.word);
+    }
     boolean r = // 上
-      backtrack(board, row - 1, column, word, index + 1, result)
+      backtrack(board, row - 1, column, curr, result)
         // 下
-        || backtrack(board, row + 1, column, word, index + 1, result)
+        || backtrack(board, row + 1, column, curr, result)
         // 左
-        || backtrack(board, row, column - 1, word, index + 1, result)
+        || backtrack(board, row, column - 1, curr, result)
         // 右
-        || backtrack(board, row, column + 1, word, index + 1, result);
+        || backtrack(board, row, column + 1, curr, result);
     board[row][column] = c;
     return r;
   }
+
+  public static class Trie {
+    Trie[] tree;
+    boolean isEnd;
+    String word;
+
+    Trie() {
+      tree = new Trie[26];
+    }
+
+    Trie(String word) {
+      tree = new Trie[26];
+      addWord(word);
+    }
+
+    public void addWord(String word) {
+      Trie[] temp = tree;
+      char[] chars = word.toCharArray();
+      for (int i = 0; i < chars.length; i++) {
+        char c = chars[i];
+        Trie curr = temp[c - 'a'];
+        if (Objects.isNull(curr)) {
+          curr = new Trie();
+          temp[c - 'a'] = curr;
+        }
+        if (i == chars.length - 1) {
+          curr.isEnd = true;
+          curr.word = word;
+        }
+        temp = curr.tree;
+      }
+    }
+  }
   // end::answer[]
+
+  static void main() {
+    new _0212_WordSearchIi_2().findWords(new char[0][0], new String[]{"oath", "pea", "eat", "rain"});
+  }
 }
