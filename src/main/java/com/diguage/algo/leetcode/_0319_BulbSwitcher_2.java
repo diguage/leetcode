fdc20d695c454d4cc7020134124bda46f013bb89
diguage <leejun119@gmail.com>
2026-02-12 21:39:09 +0800

    二319

---
+++ src/main/java/com/diguage/algo/leetcode/_0319_BulbSwitcher_2.java
@@ -0,0 +1,14 @@
+package com.diguage.algo.leetcode;
+
+public class _0319_BulbSwitcher_2 {
+  // tag::answer[]
+
+  /**
+   * @author D瓜哥 · https://www.diguage.com
+   * @since 2026-02-12 21:27:03
+   */
+  public int bulbSwitch(int n) {
+    return (int) Math.sqrt(n);
+  }
+  // end::answer[]
+}
