f58a5bb5cc438fe886864205b35d5ba973bab18c
diguage <leejun119@gmail.com>
2026-04-17 00:06:29 +0800

    完善643

--- src/main/java/com/diguage/algo/leetcode/_0643_MaximumAverageSubarrayI.java
+++ src/main/java/com/diguage/algo/leetcode/_0643_MaximumAverageSubarrayI.java
@@ -10,17 +10,21 @@ public class _0643_MaximumAverageSubarrayI {
   public double findMaxAverage(int[] nums, int k) {
     long sum = 0L;
     long result = Long.MIN_VALUE;
-    for (int i = 0; i < k; i++) {
+    // 使用单个循环，效率似乎略低
+    for (int i = 0; i < nums.length; i++) {
+      // 1. 进入窗口
       sum += nums[i];
-    }
-    result = Math.max(result, sum);
-    for (int i = k; i < nums.length; i++) {
-      sum += nums[i];
-      sum -= nums[i - k];
+      if (i < k - 1) {
+        continue;
+      }
+      // 2. 更新答案
       result = Math.max(result, sum);
+      // 3. 离开窗口
+      sum -= nums[i - k + 1];
     }
     return 1.0D * result / k;
   }
+
   // end::answer[]
   static void main() {
     new _0643_MaximumAverageSubarrayI()
