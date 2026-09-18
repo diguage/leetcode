51dab06e8b5c310e369de16a7c3cae58d355617e
diguage <leejun119@gmail.com>
2026-09-19 00:03:26 +0800

    更正一下：六刷416

--- src/main/java/com/diguage/algo/leetcode/_0416_PartitionEqualSubsetSum_6.java
+++ src/main/java/com/diguage/algo/leetcode/_0416_PartitionEqualSubsetSum_6.java
@@ -2,7 +2,6 @@ package com.diguage.algo.leetcode;
 
 public class _0416_PartitionEqualSubsetSum_6 {
   // tag::answer[]
-
   /**
    * @author D瓜哥 · https://www.diguage.com
    * @since 2026-09-18 23:28:48
@@ -22,15 +21,15 @@ public class _0416_PartitionEqualSubsetSum_6 {
     if ((sum & 1) == 1 || max > target) {
       return false;
     }
-    boolean[][] dp = new boolean[n + 1][target + 1];
+    boolean[][] dp = new boolean[target + 1][n + 1];
     dp[0][0] = true;
-    for (int i = 0; i < n; i++) {
-      int num = nums[i];
-      for (int j = 0; j <=target; j++) {
-        dp[i + 1][j] = dp[i][j] || j >= num && dp[i][j - num];
+    for (int i = 0; i <= target; i++) {
+      for (int j = 0; j < n; j++) {
+        int num = nums[j];
+        dp[i][j + 1] = dp[i][j] || i >= num && dp[i - num][j];
       }
     }
-    return dp[n][target];
+    return dp[target][n];
   }
   // end::answer[]
 }
